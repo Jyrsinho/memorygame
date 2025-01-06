baed008fae29fc0cd0406e6001da34c5db8b2384
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-06 17:02:57 +0200

    added json file containing 10 polish words

--- src/app.js
+++ src/app.js
@@ -1,25 +1,45 @@
-//Generate 4x4 grid of images
 
 var numberOfCards = 2;
 
-const newGameButton = document.getElementById("new game");
+const startGameButton = document.getElementById("start game");
 const addCardsButton = document.getElementById("add cards");
+const cardsArea = document.getElementById("cards area");
+const numberOfCardsDisplay = document.getElementById("number of cards");
 
-newGameButton.addEventListener("click", clickNewGame)
+startGameButton.addEventListener("click", clickStartGame)
 addCardsButton.addEventListener("click", clickAddCards);
 
 
 /*
 Alerts the user that the new game button has been clicked.
  */
-function clickNewGame() {
-    alert("new game clicked");
+function clickStartGame() {
+    addCards();
+
 }
 
 function clickAddCards() {
-    alert("add a new card");
+    numberOfCards = numberOfCards + 2;
+    updateNumberOfCardsDisplay();
 }
 
 
 // TODO: Create a function that sets a given amount of cards to the board
+function addCards() {
+    for (let i = 0; i < numberOfCards; i++) {
+        const newCard = document.createElement("img")
+        cardsArea.appendChild(newCard);
+    }
+}
+
+function updateNumberOfCardsDisplay() {
+    numberOfCardsDisplay.innerHTML = numberOfCards;
+}
+
+window.onload = function () {
+    addCards();
+    updateNumberOfCardsDisplay();
+}
+
+
 
