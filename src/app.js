2d31e5f455a576a355b896680744c6f8a38f3822
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-06 15:59:10 +0200

    interactivity for new game button

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+//Generate 4x4 grid of images
+
+var numberOfCards = 2;
+
+const newGameButton = document.getElementById("new game");
+
+newGameButton.addEventListener("click", clickNewGame)
+
+
+/*
+Alerts the user that the new game button has been clicked.
+ */
+function clickNewGame() {
+    alert("Button clicked!");
+}
+
+
+// TODO: Create a function that sets a given amount of cards to the board
+
