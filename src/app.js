3b6544b6340a0ee5532a26468020612d5b7ec646
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-07 21:26:04 +0200

    Gives a point when opened two of the same

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 "use strict";
 
-let numberOfCards = 2;
+let firstGuess = '';
+
+let numberOfPairs = 1;
 let numberOfOpenedCards = 0;
-const cardbackgroundImage = "assets/backpattern.png";
+const cardBackgroundImage = "assets/backpattern.png";
 let isRunning = false;
-let openedCards = [];
 let points = 0;         // points accumulated by the player
 const maximumCards = 10; // Maximum amount of cards in the game
 
@@ -37,7 +38,7 @@ const startGameButton = document.getElementById("start game");
 const stopGameButton = document.getElementById("stop game");
 const addCardsButton = document.getElementById("add cards");
 const cardsArea = document.getElementById("cards area");
-const numberOfCardsDisplay = document.getElementById("number of cards");
+const numberOfCardsDisplay = document.getElementById("number of pairs");
 const body = document.body;
 const pointCounter = document.getElementById("pointsCounter");
 
@@ -60,7 +61,7 @@ function clickStopGame() {
 
 function clickAddCards() {
     if (!isRunning) {
-        numberOfCards = numberOfCards + 2;
+        numberOfPairs++;
         updateNumberOfCardsDisplay();
         addCards();
     }
@@ -71,36 +72,36 @@ function clickAddCards() {
 // TODO: Create a function that sets a given amount of cards to the board
 function addCards() {
     cardsArea.innerHTML = "";
-    for (let i = 0; i < numberOfCards; i++) {
-        let newCard = document.createElement("img")
-        let cardImage = document.createElement("img")
-        newCard.src = cardbackgroundImage;
-        cardImage.src = "assets/pattern" + cardNumber +".png";
-        newCard.addEventListener ("click", function () {
+    for (let i = 0; i < numberOfPairs; i++) {
+        for (let j = 0; j < 2; j++) {
+            let newCard = document.createElement("img")
+            let cardImage = document.createElement("img")
+            newCard.src = cardBackgroundImage;
+            cardImage.src = images.at(i).img;
+            newCard.addEventListener ("click", function () {
             openCard(newCard, cardImage);
-        });
-        cardsArea.appendChild(newCard);
-        if (i % 2 === 1) {
-            cardNumber++;
+            });
+            cardsArea.appendChild(newCard);
         }
     }
 }
 
 function openCard(openedCard, cardImage) {
         if (isRunning) {
-            if (openedCard.src.includes(cardbackgroundImage) && numberOfOpenedCards < 2) {
+            if (openedCard.src.includes(cardBackgroundImage) && numberOfOpenedCards < 2) {
                 openedCard.src = cardImage.src
                 numberOfOpenedCards++;
-                openedCards.push(openedCard);
+
 
             } else if (openedCard.src.includes(cardImage.src))  {
-                openedCard.src = cardbackgroundImage;
+                openedCard.src = cardBackgroundImage;
                 numberOfOpenedCards--;
-                openedCards.pop(openedCard)
             }
         }
+        if (numberOfOpenedCards ===1) firstGuess = openedCard.src;
+
     if (numberOfOpenedCards === 2) {
-        checkForPair();
+        checkForPair(openedCard);
     }
 
 }
@@ -108,19 +109,18 @@ function openCard(openedCard, cardImage) {
 /**
  * Checks whether the currently opened cards are pairs
  */
-function checkForPair() {
-    if (openedCards.at(0) === openedCards.at(1)) {
-        points += 1;
-        updatePointsDisplay()
-        openedCards.pop();
-        openedCards.pop();
-    }
+function checkForPair(openedCard) {
+   if (openedCard.src.includes(firstGuess)) {
+       points++
+   }
+    updatePointsDisplay()
 }
 
 
 function updateNumberOfCardsDisplay() {
-    numberOfCardsDisplay.innerHTML = numberOfCards;
+    numberOfCardsDisplay.innerHTML = numberOfPairs;
 }
+
 
 function updatePointsDisplay() {
     pointCounter.innerHTML = points;
