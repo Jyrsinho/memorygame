db55f3e3eb0d3b2565253e44ab133d4755380b28
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-07 17:12:20 +0200

    Can only open two cards at a time.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 
 let numberOfCards = 2;
+let numberOfOpenedCards = 0;
 const cardbackgroundImage = "assets/backpattern.png";
-
+let isRunning = false;
 
 
 const startGameButton = document.getElementById("start game");
@@ -9,7 +10,9 @@ const stopGameButton = document.getElementById("stop game");
 const addCardsButton = document.getElementById("add cards");
 const cardsArea = document.getElementById("cards area");
 const numberOfCardsDisplay = document.getElementById("number of cards");
-let isRunning = false;
+const body = document.body;
+
+
 
 startGameButton.addEventListener("click", clickStartGame)
 addCardsButton.addEventListener("click", clickAddCards);
@@ -20,11 +23,14 @@ Alerts the user that the new game button has been clicked.
  */
 function clickStartGame() {
     isRunning = true;
+    body.style.backgroundColor = "white";
+
 
 }
 
 function clickStopGame() {
     isRunning = false;
+    body.style.backgroundColor = "lightblue";
 }
 
 function clickAddCards() {
@@ -39,27 +45,32 @@ function clickAddCards() {
 // TODO: Create a function that sets a given amount of cards to the board
 function addCards() {
     cardsArea.innerHTML = "";
+    let cardNumber = 0;
     for (let i = 0; i < numberOfCards; i++) {
         let newCard = document.createElement("img")
         let cardImage = document.createElement("img")
         newCard.src = cardbackgroundImage;
-        cardImage.src = "assets/pattern1.png";
+        cardImage.src = "assets/pattern" + i +".png";
         newCard.addEventListener("click", function () {
             openCard(newCard, cardImage);
         });
         cardsArea.appendChild(newCard);
+        if (i % 2 === 0) {
+            cardNumber++;
+        }
     }
 }
 
 function openCard(openedCard, cardImage) {
-        if (openedCard.src.includes(cardbackgroundImage)) {
-            openedCard.src = cardImage.src
-        } else {
-            openedCard.src = cardbackgroundImage;
+        if (isRunning) {
+            if (openedCard.src.includes(cardbackgroundImage) && numberOfOpenedCards < 2) {
+                openedCard.src = cardImage.src
+                numberOfOpenedCards++;
+            } else if (openedCard.src.includes(cardImage.src))  {
+                openedCard.src = cardbackgroundImage;
+                numberOfOpenedCards--;
+            }
         }
-
-
-
 }
 
 
