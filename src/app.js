8a33181192166bc67250fe5be70f021b089626a4
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-07 20:37:08 +0200

    Added images to an array

--- src/app.js
+++ src/app.js
@@ -3,6 +3,33 @@ let numberOfCards = 2;
 let numberOfOpenedCards = 0;
 const cardbackgroundImage = "assets/backpattern.png";
 let isRunning = false;
+let openedCards = [];
+let points = 0;         // points accumulated by the player
+const maximumCards = 10; // Maximum amount of cards in the game
+
+const images = [
+    {
+        'name': 'baby-turtle',
+        'img': 'assets/baby_turtle.png',
+    },
+    {
+        'name': 'mars-rover',
+        'img': 'assets/mars-rover.png',
+    },
+    {
+        'name': 'pattern1',
+        'img': 'assets/pattern1.png',
+    },
+    {
+        'name': 'rooster',
+        'img': 'assets/rooster.png',
+    },
+    {
+        'name': 'socklady',
+        'img': 'assets/socklady.png',
+    }
+]
+
 
 
 const startGameButton = document.getElementById("start game");
@@ -11,6 +38,7 @@ const addCardsButton = document.getElementById("add cards");
 const cardsArea = document.getElementById("cards area");
 const numberOfCardsDisplay = document.getElementById("number of cards");
 const body = document.body;
+const pointCounter = document.getElementById("pointsCounter");
 
 
 
@@ -24,8 +52,6 @@ Alerts the user that the new game button has been clicked.
 function clickStartGame() {
     isRunning = true;
     body.style.backgroundColor = "white";
-
-
 }
 
 function clickStopGame() {
@@ -50,12 +76,12 @@ function addCards() {
         let newCard = document.createElement("img")
         let cardImage = document.createElement("img")
         newCard.src = cardbackgroundImage;
-        cardImage.src = "assets/pattern" + i +".png";
+        cardImage.src = "assets/pattern" + cardNumber +".png";
         newCard.addEventListener("click", function () {
             openCard(newCard, cardImage);
         });
         cardsArea.appendChild(newCard);
-        if (i % 2 === 0) {
+        if (i % 2 === 1) {
             cardNumber++;
         }
     }
@@ -66,16 +92,39 @@ function openCard(openedCard, cardImage) {
             if (openedCard.src.includes(cardbackgroundImage) && numberOfOpenedCards < 2) {
                 openedCard.src = cardImage.src
                 numberOfOpenedCards++;
+                openedCards.push(openedCard);
+
             } else if (openedCard.src.includes(cardImage.src))  {
                 openedCard.src = cardbackgroundImage;
                 numberOfOpenedCards--;
+                openedCards.pop(openedCard)
             }
         }
+    if (numberOfOpenedCards === 2) {
+        checkForPair();
+    }
+
+}
+
+/**
+ * Checks whether the currently opened cards are pairs
+ */
+function checkForPair() {
+    if (openedCards.at(0) === openedCards.at(1)) {
+        points += 1;
+        updatePointsDisplay()
+        openedCards.pop();
+        openedCards.pop();
+    }
 }
 
 
 function updateNumberOfCardsDisplay() {
     numberOfCardsDisplay.innerHTML = numberOfCards;
+}
+
+function updatePointsDisplay() {
+    pointCounter.innerHTML = points;
 }
 
 
